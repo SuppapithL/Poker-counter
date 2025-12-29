6f47ecc2e5a25b2b0fbd9510bacc13bd3467ad72
Tomac <Suppapith@gmail.com>
2025-12-29 18:46:19 +0100

    stupid clear console func

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Poker-counter
 Teaching stupid python 4 ma monke brain in free time (Mostly because IDK Poker Rule)
+
+
+PS>>> WTF IS POKER EVEN ABOUT?
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import sys, subprocess
+
 class player:
     def __init__(self, name,  number, point):
         self.num = number
         self.point = point
         self.name = name
 
+def clearconsole():
+    os = sys.platform
+    if os == 'linux' or 'darwin':
+        command = 'clear'
+    elif os == 'win32':
+        command = 'cls'
+    subprocess.run(command, shell = True)
+
 class game:
-    def _init_(self, Ante = 0):
+    def _init_(self, Ante = 0, ):
         self.Ante = Ante
+        self.mainpot = Ante
 
 def Table():
     print ("This is Game")
@@ -18,11 +29,12 @@ def checkstatus(Players):
 Playercount = int(input("Please input number of players: "))
 Players =  ["x"] * Playercount
 startpoints = int(input("Please enter start money: "))
-Ante = int(input("Please enter Ante of this round: "))
 
 for Playernum in range(Playercount):
-    name = input("Please put Name of player: ")
+    print(f"Please put Name of player{Playernum}: ")
+    name = input()
     Players[Playernum] = player(name, Playernum, startpoints)
 
 input("Enter to continue")
+clearconsole()
 checkstatus(Players)
