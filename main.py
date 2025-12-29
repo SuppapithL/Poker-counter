a188f3a8f4dade88bb23f7b17c7452e383c5053e
Tomac <Suppapith@gmail.com>
2025-12-29 05:37:17 +0100

    At least it work a :/

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 class player:
-    def _init_(self, name,  number, point):
+    def __init__(self, name,  number, point):
         self.num = number
         self.point = point
         self.name = name
+
 class game:
     def _init_(self, Ante = 0):
         self.Ante = Ante
-Playercount = int(input("Please input number of players:"))
-Players =  ["x"] * Playercount
-startpoints = int(input("Please enter start money:"))
-Ante = int(input("Please enter Ante of this round:"))
-for Playernum in range(Playercount):
-    name = input("Please put Name of player")
-    Players[Playernum] = player(name, Playernum, startpoints)
-    input("Enter to continue")
-    table()
+
 def Table():
     print ("This is Game")
+
+def checkstatus(Players):
+    for identity in Players:
+        print("Player",identity.name,"Num.",identity.num,"point: ",identity.point)
+
+Playercount = int(input("Please input number of players: "))
+Players =  ["x"] * Playercount
+startpoints = int(input("Please enter start money: "))
+Ante = int(input("Please enter Ante of this round: "))
+
+for Playernum in range(Playercount):
+    name = input("Please put Name of player: ")
+    Players[Playernum] = player(name, Playernum, startpoints)
+
+input("Enter to continue")
+checkstatus(Players)
