559b9cdfa0456547c2619e7d7f17d0b37fd51f7e
John.China <Suppapith@gmail.com>
2026-02-22 19:11:57 +0100

    update for table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, subprocess
+import random
 
 class player:
     def __init__(self, name,  number, point):
@@ -9,17 +10,25 @@ class player:
     def playerinfo(self):
         return f"Num: {self.num},Name: {self.name},Point: {self.point}"
 
+    def updateinfo(self, newpoint):
+        self.point = newpoint
+
 class Table:
     def __init__(self, players):
         self.players = players
         self.pot = 0
-        self.community_cards = []
-        self.deck = []
         self.turn_index = 0
+        self.dealer = random.choice(players)
+    
+    def tableinfo(self):
+        package = []
+        for player in self.players:
+            package.append(f"Num: {player.num},Name: {player.name},Point: {player.point},Dealer: {player == self.dealer}")
+        return package
 
 def clearconsole():
     os = sys.platform
-    if os == 'linux' or 'darwin':
+    if os in ('linux', 'darwin'):
         command = 'clear'
     elif os == 'win32':
         command = 'cls'
@@ -28,39 +37,39 @@ def clearconsole():
 def Lobby(Players):
     clearconsole()
     print ("This is Lobby!\nPlayers information:")
-    checkstatus(Players)
+    for identity in Players:
+        print(identity.playerinfo())
     staged = input("Please enter number of the player that will join(x,y,...):").split(",")
     Joined = []
     for Nums in staged:
-        if int(Nums) in Players:
+        if int(Nums) < len(staged):
             Joined.append(Players[int(Nums)])
     clearconsole()
     Tables(Joined)
 
-def checkstatus(Players):
-    for identity in Players.values():
-        print(identity.playerinfo())
-
 def Setup():
     Playercount = int(input("Please input number of players: "))
     Players =  {}
+    Playerlist = []
     startpoints = int(input("Please enter start money: "))
-
     for Playernum in range(Playercount):
         print(f"Please put Name of player{Playernum}: ")
         name = input()
         Players[Playernum] = player(name, Playernum, startpoints)
     clearconsole()
-    checkstatus(Players)
+    for identity in Players.values():
+        Playerlist.append(identity)
+    for identity in Playerlist:
+        print(identity.playerinfo())
     input("Enter to continue if information is correct.If not, control c to cancle")
-    return Players
+    return Playerlist
 
 def Tables(Joined):
     clearconsole()
     Tabl = Table(Joined)
     print("This is Table, Joined player:")
-    for PPL in Joined:
-        print(PPL.playerinfo())
+    for element in Tabl,tableinfo():
+        print(element)
 
 def main():
     Players = Setup()
